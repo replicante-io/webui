f0b80556643d8e70087b9e87765a1a3e0f0575f3
Stefano Pogliani <stefano@spogliani.net>
2018-04-22 19:13:56 +0100

    Minimal readme for minimal UI

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 Replicante WebUI
 ================
-TODO
+Bare bones web-based user interface for Replicante.
+Its main purpose is to visualise the current state of your clusters
+and what is happening to them.
 
 
-Project setup notes
--------------------
-- Commit pre-compiled `dist/`? (would allow clone && start "install")
+Install
+-------
+```bash
+git clone https://github.com/replicante-io/webui.git
+cd webui/
+npm install
+npm run build
+```
+
+Usage
+-----
+```bash
+export REPLI_BACKEND_ROOT="http://localhost:16016"
+npm run server
+```
--- client/src/component/dashboard/index.js
+++ client/src/component/dashboard/index.js
@@ -39,30 +39,9 @@ class Dashboard extends React.Component<{}> {
     );
   }
 
-  renderOverview() {
-    return (
-      <div className="row space-below">
-        <div className="col">
-          <div className="card">
-            <div className="card-header">
-              Overview of your systems
-              <FetchButton id="overview" request={REPLACE_ME} />
-            </div>
-
-            <div className="card-body text-center">
-              <p className="card-text large-icon"><i className="fas fa-chart-area"></i></p>
-              <p className="card-text">No data available</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   render() {
     return (
       <div className="overview-container">
-        {this.renderOverview()}
         {this.renderEvents()}
         <Clusters />
       </div>
